0f919e607494fcc20f17d923870794d337b6d150
Abhinav shukla <124790218+Abhinavshukla12@users.noreply.github.com>
2024-06-27 23:15:36 +0530

    review model update

--- app/Models/ProjectModel/ReviewModel.php
+++ app/Models/ProjectModel/ReviewModel.php
@@ -14,4 +14,22 @@ class ReviewModel extends Model
     {
         return $this->where('book_id', $book_id)->findAll();
     }
+
+    public function getLatestReviewsByBook($book_id, $limit = 3)
+    {
+        return $this->where('book_id', $book_id)->orderBy('created_at', 'DESC')->limit($limit)->findAll();
+    }
+
+    public function getAverageRating($book_id)
+    {
+        $this->selectAvg('rating');
+        $this->where('book_id', $book_id);
+        $query = $this->get();
+        return $query->getRow()->rating;
+    }
+
+    public function getLatestHighRatingReviews($limit = 4)
+    {
+        return $this->where('rating >=', 4)->orderBy('created_at', 'DESC')->limit($limit)->findAll();
+    }
 }
